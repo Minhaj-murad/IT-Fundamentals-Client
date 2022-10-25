55ca5db8d07ddaba7f7e9ac171de60f34616e737
Minhajmurad <minhajmurad421@gmail.com>
2022-10-25 19:17:02 +0600

    simple signup,signi in and header section added

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
     return (
-        <div>
-            <h1>this is login page</h1>
+        <div >
+            <form className='w-96 mx-auto h-auto  shadow-2xl text-center mt-4'>
+               
+                <div className='mt-4'>
+                <h1 className='text-2xl '>Email</h1>
+                <input className='border border-gray-900 shadow-2xl' type="email" name="email" id="" placeholder='Your Email' required/>
+                </div>
+                <div className='mt-4'>
+                <h1 className='text-2xl '>Password</h1>
+                <input className='border border-gray-900 shadow-2xl' type="password" name="password" id="" placeholder='Your password' required/>
+                </div>
+                <button className='bg bg-amber-700 rounded-xl my-4 py-1 px-2 font-semibold ' type="submit">Log in </button>
+                <br />
+                <p>Don't have an account? Please <Link to='/signup' className='text-blue-800 font-semibold'>Sign Up  </Link> First</p>
+            </form>
         </div>
     );
 };
--- src/Components/Signup/Signup.js
+++ src/Components/Signup/Signup.js
@@ -1,11 +1,32 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Signup = () => {
     return (
-        <div>
-            <h1>this is signup page</h1>
-        </div>
-    );
+        <div >
+        <form className='w-96 mx-auto h-auto  shadow-2xl text-center mt-4'>
+            <div className='mt-4'>
+            <h1 className='text-2xl '>Your Name</h1>
+            <input className='border border-gray-900 shadow-2xl' type="name" name="name" id="" placeholder='Your name' required/>
+            </div>
+            <div className='mt-4'>
+            <h1 className='text-2xl '>Your PhotoURL</h1>
+            <input className='border border-gray-900 shadow-2xl' type="text" name="photoURL" id="" placeholder='Your PhotoURL' required/>
+            </div>
+            <div className='mt-4'>
+            <h1 className='text-2xl '>Email</h1>
+            <input className='border border-gray-900 shadow-2xl' type="email" name="email" id="" placeholder='Your Email' required/>
+            </div>
+            <div className='mt-4'>
+            <h1 className='text-2xl '>Password</h1>
+            <input className='border border-gray-900 shadow-2xl' type="password" name="password" id="" placeholder='Your password' required/>
+            </div>
+            <button className='bg bg-amber-700 rounded-xl my-4 py-1 px-2 font-semibold ' type="submit">Register </button>
+            <br />
+            <p>Already have an account? Please <Link to='/login' className='text-green-400 font-semibold'>Log In </Link></p>
+        </form>
+    </div>
+);
 };
 
 export default Signup;

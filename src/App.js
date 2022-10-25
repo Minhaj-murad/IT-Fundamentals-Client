81869d77b28625dda4260c3925cc0556c7e9c9b4
Minhajmurad <minhajmurad421@gmail.com>
2022-10-25 17:23:46 +0600

    simple routes created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,50 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Blog from "./Blog/Blog";
+import Courses from "./Courses/Courses";
+import FAQ from "./FAQ/FAQ";
+import Home from "./Home/Home";
+import Main from "./Main/Main";
 
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Main></Main>,
+      children: [
+        {
+          path: '/',
+          element: <Home></Home>
+        },
+        {
+          path: '/home',
+          element: <Home></Home>
+        },
+        {
+          path: '/courses',
+          element: <Courses></Courses>
+        },
+        {
+          path: '/faq',
+          element: <FAQ></FAQ>
+        },
+        {
+          path: '/blog',
+          element: <Blog></Blog>
+        }
+
+      ]
+    }
+  ])
+
+
+
+
   return (
-    <div className="App">
-     
+    <div >
+      <RouterProvider router={router}>
+
+      </RouterProvider>
     </div>
   );
 }
